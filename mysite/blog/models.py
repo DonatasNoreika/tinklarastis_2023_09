61277c4212f77905e33fbda5dbae509d3f5df59d
Donatas Noreika <donoras@gmail.com>
2023-09-08 08:54:30 +0300

    +Galimybė palikti komentarą prisijungusiam vartotojui prie posto

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -22,3 +22,6 @@ class Comment(models.Model):
     created = models.DateTimeField(verbose_name="Sukūrimo data", auto_now_add=True)
     post = models.ForeignKey(to="Post", verbose_name="Įrašas", on_delete=models.CASCADE, related_name="comments")
     user = models.ForeignKey(to=User, verbose_name="Autorius", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        ordering = ["-created"]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.views import generic
 from .models import Post
-
+from django.views.generic.edit import FormMixin
+from .forms import CommentForm
 
 # Create your views here.
 class PostListView(generic.ListView):
@@ -11,7 +12,27 @@ class PostListView(generic.ListView):
     paginate_by = 5
 
 
-class PostDetailView(generic.DetailView):
+class PostDetailView(FormMixin, generic.DetailView):
     model = Post
     context_object_name = 'post'
     template_name = "post.html"
+    form_class = CommentForm
+
+    def get_success_url(self):
+        return reverse('post', kwargs={'pk': self.object.id})
+
+    # standartinis post metodo perrašymas, naudojant FormMixin, galite kopijuoti tiesiai į savo projektą.
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.post = self.object
+        form.save()
+        return super().form_valid(form)
+
